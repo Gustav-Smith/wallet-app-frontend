52d32bd332726af4dd93b1e3b3938bae3888bc5b
Gustav-Smith <gustavojeanpt@gmail.com>
2023-12-28 22:09:41 -0300

    feat: integrate user initial validation

---
+++ index.js
@@ -0,0 +1,29 @@
+const validateUser = async (email) => {
+  try {
+    const result = await fetch(
+      `https://mp-wallet-app-api.herokuapp.com/users?email=${email}`
+    );
+    const user = await result.json();
+    return user;
+  } catch (error) {
+    return { error };
+  }
+};
+
+
+const onClickLogin = async () => {
+  const email = document.getElementById('input-email').value;
+  if (email.lenght < 5 || !email.includes("@")) {
+    alert("Email inválido!");
+    return;
+  }
+  const result = await validateUser(email);
+  if (result.error) {
+    alert("Falha ao validar e-mail.");
+    return;
+  }
+  localStorage.setItem("@walletApp:userEmail", result.email);
+  localStorage.setItem("@walletApp:userName", result.name);
+  localStorage.setItem("@walletApp:userId", result.id);
+  window.open('/src/pages/home/index.html', "_self");
+};
---
+++ src/pages/home/index.js
@@ -0,0 +1,4 @@
+window.onload = () => {
+  const email = localStorage.getItem('@walletApp:userEmail');
+  console.log(email); 
+}
